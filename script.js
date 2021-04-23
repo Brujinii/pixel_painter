1aef7049e0ac245c1ee178d5e9db7a669d7d2a62
Brujinii <kevin.gonci@gmail.com>
2021-04-23 11:22:26 -0400

    Added toggleable grid lines

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById("grid");
 let is_drawing = false;
-let color = "black"
 
+// Create Grid to Specified Dimension
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -10,8 +10,9 @@ function makeRows(rows, cols) {
         container.appendChild(cell).className = "grid-item";
     };
 };
-makeRows(16, 16);
+makeRows(64, 64);
 
+// Drawing Code
 container.addEventListener('mouseleave', () => {
     is_drawing = false
 })
@@ -19,28 +20,58 @@ container.addEventListener('mouseleave', () => {
 const pixels = document.querySelectorAll(".grid-item");
 
 pixels.forEach((pixel) => {
-    console.log("Hello")
-    pixel.style.backgroundColor = "white"
-    pixel.addEventListener('mousedown', () => {
+    pixel.style.backgroundColor = "white"  // default background color
+    pixel.addEventListener('mousedown', () => {  // when mousedown, draw in square
         is_drawing = true
         pixel.style.backgroundColor = color
     });
-    pixel.addEventListener('mouseup', () => {
+    pixel.addEventListener('mouseup', () => {   // stop drawing on mouseup
         is_drawing = false
     });
-    pixel.addEventListener('mouseenter', () => {
+    pixel.addEventListener('mouseenter', () => {   // if is_drawing == true when mouse enters, draw
         if (is_drawing) {
             pixel.style.backgroundColor = color
         };
     });
 });
 
+// Clear function
 const clear = document.getElementById('clear') 
 clear.addEventListener('click', () => {
-    let clear_doc = confirm("Would you really like to clear?")
+    let clear_doc = confirm("Would you really like to clear?")  // make sure that the user would like to clear
     if (clear_doc) {
         pixels.forEach((pixel) => {
             pixel.style.backgroundColor = "white"
         })
+    }
+})
+
+// Color Picker
+const color_picker = document.getElementById("colorpicker");
+let color = color_picker.value;
+
+color_picker.addEventListener('input', () => {
+    color = color_picker.value
+});
+
+// Toggle grid lines
+const grid_lines = document.getElementById('toggle-grid')
+let grid_lines_drawn = true
+grid_lines.addEventListener('click', () => {
+    if (grid_lines_drawn) {
+        pixels.forEach((pixel) => {
+            pixel.style.border = "none"
+            pixel.style.padding = '1px'
+        })
+        container.style.columnGap = "0px"
+        container.style.rowGap = "0px"
+        grid_lines_drawn = false
+    } else {
+        pixels.forEach((pixel) => {
+            pixel.style.padding = '0px'
+        })
+        container.style.columnGap = "2px"
+        container.style.rowGap = "2px"
+        grid_lines_drawn = true    
     }
 })
